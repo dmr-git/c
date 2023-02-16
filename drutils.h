c998ed5dabf20c2fb3e2081d013d86b1c5c7d66d
dmr-git <denrichter@gmail.com>
2023-02-16 13:46:54 -0500

    changes

--- drutils.h
+++ drutils.h
@@ -8,11 +8,27 @@
 /***********************/
 /* Constants           */
 /***********************/
-#define COLOR_RED "\e[31m"
-#define COLOR_OFF "\e[m"
+#define BOLD "\e[1m"
+#define BLINK "\e[5m"
+#define BLACK "\e[30m"
+#define RED "\e[31m"
+#define GREEN "\e[32m"
+#define YELLOW "\e[33m"
+#define BLUE "\e[34m"
+#define MAGENTA "\e[35m"
+#define CYAN "\e[36m"
+#define BRIGHTWHITE "\e[37m"
+#define UNDERLINE "\e[4m"
+
+#define OFF "\e[m"
+
+/*********************/
+/* enum Declarations */
+/**********************/
+enum eDays { SUN, MON, TUE, WED, THU, FRI, SAT };
 
 /***********************/
-/* Struct Declarations */
+/* struct Declarations */
 /***********************/
 
 /*************************/
@@ -23,6 +39,9 @@ extern float  CelsiusToFahrenheit(float tempC);
 extern void   Delay(int numSeconds);
 extern void   FlowerBox(const char* str);
 extern float  FahrenheitToCelsius(float tempF);
+extern void   IntToDayStr(int dayInt, char* dayStr);
+extern int    IsLeapYear(int year);
+extern int    MakeInt(char digitChar);
 extern void   MakeLower(char * str);
 extern void   MakeUpper(char * str);
 extern char*  MakeStringInHeap(const char* str);
